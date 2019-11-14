38a7d9a295ebd1973cc661b45baac8d0d55bba65
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2019-11-14 16:21:19 +0100

    ..

--- app/Main.hs
+++ app/Main.hs
@@ -1,40 +1,11 @@
--- module Main where
+module Main where
 
 import Lib
-import Data.List.Split
-import Control.Monad
--- main :: IO ()
 
-splitStr :: String -> [String]
-splitStr str = splitOn " " str
+main :: IO ()
 
-
--- function factorial (n) {
---   if (n < 2) {
---     return 1;
---   } else {
---     return n * factorial(n-1);
---   }
--- }
-
-recsum :: Int -> Int -> Int
-recsum n t = if n == 0 then t else recsum (n-1) (t+n)
-
-nrecsum :: Int -> Int
-nrecsum n = if n == 1 then n else n + (nrecsum (n-1))
-
-main = print (recsum 500000000 0)
--- main = print (nrecsum 500000000)
-
--- main = do
---     putStrLn "Please write some shit"
---     args <- getLine
---     putStrLn (splitStr args!!1)
-
--- main = print ("n:" ++ (show (inc 20)))
--- main  = (print "Micke") >> (print "Ahlinder")
-
--- main = do  
---   putStrLn "Hello, what's your name?"  
---   name <- getLine  
---   putStrLn ("Hey " ++ name ++ ", you rock!")  
+main = do
+    putStrLn "Please write something nice"
+    args <- getLine
+    putStrLn (split_string args!!0)
+    putStrLn "Bye!"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,15 @@
 module Lib
 ( 
-  inc,
-  someFunc
+  recsum,
+  recsum_nontail,
+  split_string
 ) where
 
--- someFunc :: IO ()
-someFunc = putStrLn "Wesllo!"
+recsum :: Int -> Int -> Int
+recsum n t = if n == 0 then t else recsum (n-1) (t+n)
 
-inc :: Int -> Int     
-inc x = x + 1  
+recsum_nontail :: Int -> Int
+recsum_nontail n = if n == 1 then n else n + (recsum_nontail (n-1))
+
+split_string :: String -> [String]
+split_string str = words  str
